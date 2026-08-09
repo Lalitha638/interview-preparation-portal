3ffdf3046c34e5ce6a60bdaa029730100584a260
Lalitha638 <lalithalalithat14@gmail.com>
2026-08-09 12:29:24 +0530

    Add user registration and database setup

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
+import database
+
 app=FastAPI()
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
 @app.get("/")
 def home():
     return{'message':'Welcome to Interview Preparation Portal'}
+
+@app.post('/register')
+def register_user(user: User):
+
+    connection=sqlite3.connect('interview_portal.db')
+    cursor=connection.cursor()
+
+    cursor.execute(
+        'INSERT INTO users(name, email, password) VALUES (?, ?, ?)',
+        (user.name, user.email, user.password)
+    )
+    connection.commit()
+    connection.close()
+
+    return {
+        'message':'User registered successfully',
+    'name':user.name,
+    'email':user.email
+    }
